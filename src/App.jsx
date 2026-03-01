2194103ec577a3f495f03bc322ca949e105d346f
shuvo903 <saifulislamshuvoa123@gmail.com>
2026-03-01 20:52:57 +0600

    added users component

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,41 @@
 import './App.css'
 import Batsman from './Batsman';
 import Counter from './Counter';
+import Users from './Users';
 
 function App() {
 
-function handleClick(){
-alert('I am Clicked')
-}
+  function handleClick() {
+    alert('I am Clicked')
+  }
 
-// const handleClick3 = () => {
-//   alert('Clicked 3')
-// }
+  // const handleClick3 = () => {
+  //   alert('Clicked 3')
+  // }
 
-const handleAdd5 = (num) => {
-  const newNum = num + 5;
-  alert(newNum);
-}
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  }
 
   return (
     <>
-   
+
       <h3>Vite + React</h3>
 
+
+      <Users></Users>
+
+      
       <Batsman></Batsman>
 
       <Counter></Counter>
 
-<button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick}>Click Me</button>
 
-{/* <button onClick={handleClick3}>Click Me 3</button> */}
+      {/* <button onClick={handleClick3}>Click Me 3</button> */}
 
-<button onClick={() => handleAdd5(10)}>Click Me 4</button>
+      <button onClick={() => handleAdd5(10)}>Click Me 4</button>
     </>
   )
 }
---
+++ src/Users.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div className='card'>
+            <h3>Users: </h3>
+            <p>sdfas sgadfga sd</p>
+        </div>
+    );
+};
+
+export default Users;
